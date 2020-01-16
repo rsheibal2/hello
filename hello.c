85b065c54d5483f9f1e9d096c87b3f44d85796a2
Richard Sheibal <59897195+rsheibal2@users.noreply.github.com>
2020-01-16 15:20:31 -0600

    Create hello.c
    This is my first code within a repository. I added a print() line for the Hack session.

---
+++ hello.c
@@ -0,0 +1,16 @@
+/*
+* Richard Sheibal
+* 1/16/20
+* CSCE 155E
+* Create a repository
+*/
+
+#include <stdio.h>
+
+int main(void) {
+	
+	printf("Hello World!");
+	printf("My major is Computer Science!"); //added major for assignment
+	
+	return 0;
+}
